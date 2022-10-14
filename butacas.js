90cee6b76af39612f2df2fa96e5413873df22e66
MonitoCoder <joacofernan@hotmail.com>
2022-10-14 10:04:14 -0300

    asdsad

--- butacas.js
+++ butacas.js
@@ -19,10 +19,20 @@
 
 function Cambiar(clicked_id)
 {
+    var t=0; 
+    var i=0;
+    var tickets = document.getElementsByClassName("compra_entrada");
+    for(var i=0;i<4;i++){
+        if (tickets[i].style.display == "block"){
+            t++;
+        }
+    }
     var color = document.getElementById(clicked_id).src;
     console.log(color);
     if(document.getElementById(clicked_id).src === "file:///C:/Users/minec/Documents/GitHub/CineMaxFun/imgs/event_seat.svg"){
         document.getElementById(clicked_id).src = "imgs/butaca_seleccionada.svg";
+        tickets[t].style.display = "block";
+
     }  
     else {
         document.getElementById(clicked_id).src = "imgs/event_seat.svg";
